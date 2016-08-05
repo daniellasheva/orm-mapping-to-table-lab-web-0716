e09766064bc341cfe353e3427de60f6a0c681627
daniellasheva <daniellasheva@gmail.com>
2016-08-05 14:54:23 -0400

    initial commit

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,49 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+
+  attr_accessor :name, :grade 
+  attr_reader :id
+
+  def initialize (id= nil, name, grade)
+    @name=name
+    @grade=grade
+    @id=id
+  end
+
+  def self.create_table
+    sql= <<-SQL
+      CREATE TABLE students(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT);
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+    SQL
+    DB[:conn].execute (sql)
+  end
+
+  def save 
+    sql = <<-SQL
+      INSERT INTO students(name,grade) VALUES (?, ?)
+      SQL
+    DB[:conn].execute(sql, name, grade)
+     
+     sql = <<-SQL
+      SELECT last_insert_rowid() FROM students
+      SQL
+    @id= DB[:conn].execute(sql).first.first #returns array of arrays. First value of "first"is an array, and then we choose the first of that array
+  end
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
   
 end
